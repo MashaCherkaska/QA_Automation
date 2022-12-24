68f8757788ce97438477c9eb5400a4e8fc0a327d
Marianna Cherkaska <mashacherkass@gmail.com>
2022-12-24 21:39:50 +0100

    add HW3 to lesson3

---
+++ src/main/java/com/hillel/lessons/lesson3/HW3.java
@@ -0,0 +1,9 @@
+package com.hillel.lessons.lesson3;
+
+public class HW3 {
+    public static void main(String[] args) {
+        int x = 500;
+        int y = 10;
+        System.out.println(x/y);
+    }
+}
